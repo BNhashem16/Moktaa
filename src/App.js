97dfb3259fdc09c922d483ace9620c15926da484
Ahmed Hashem <hashem.codes@gmail.com>
2023-10-23 02:26:40 +0300

    filter barcodes

--- src/App.js
+++ src/App.js
@@ -5,11 +5,22 @@ import { QrScanner } from '@yudiel/react-qr-scanner';
 
 function App() {
   const [qrResult, setQrResult] = useState(null);
+  const barcodes = ["649528906526"];
+
+  const handleBarcodeScan = (result) => {
+    setQrResult(result);
+
+    if (barcodes.includes(result)) {
+      alert(`Barcode ${result} exists in the array!`);
+    } else {
+      console.log(`Barcode ${result} does not exist in the array.`);
+    }
+  }
 
   return (
     <div>
       <QrScanner
-        onDecode={(result) => setQrResult(result)}
+        onDecode={handleBarcodeScan}
         onError={(error) => console.log(error?.message)}
       />
       {qrResult && <div>QR Result: {qrResult}</div>}
