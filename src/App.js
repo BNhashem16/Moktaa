27b2aca994406714d0ec371e96453f41454ba5f1
Ahmed Hashem <hashem.codes@gmail.com>
2023-10-23 19:25:22 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 import { QrScanner } from "@yudiel/react-qr-scanner";
 import { Alert, AlertTitle } from "@mui/material";
 import { useTranslation } from "react-i18next";
+import { BarcodeBlackListComponent, barcodeExists, getBarcodeData } from "./BarcodeBlackListComponent";
 function App() {
   const [qrResult, setQrResult] = useState(null);
   const { t } = useTranslation();
-  const barcodes = [
-    "649528906526", // for testing
-    "6221031490569", // chipsy forno
-    "6221007083160", // cimo cono
-    "6221007022183", // Nescafe gold packet
-    "8445290212863", // Nescafe gold jar
-    "6221033177239", // Heinz mionize
-    "4601674084288", // Heinz ketchup
-  ];
+  // const barcodes = [
+  //   "649528906526", // for testing
+  //   "6221031490569", // chipsy forno
+  //   "6221007083160", // cimo cono
+  //   "6221007022183", // Nescafe gold packet
+  //   "8445290212863", // Nescafe gold jar
+  //   "6221033177239", // Heinz mionize
+  //   "4601674084288", // Heinz ketchup
+  // ];
+  const barcodes = BarcodeBlackListComponent();
 
   const handleBarcodeScan = (result) => {
     setQrResult(result);
@@ -45,21 +47,26 @@ function App() {
     <Alert severity="success">
       <AlertTitle>{t("success")}</AlertTitle>
       {t("this_is_success_alert")} — <strong>{t("product_whitelist")}</strong>
+      <div>
+            <p>Barcode: {qrResult}</p>
+            <p>Product Name: {getBarcodeData(qrResult).product_name}</p>
+          </div>
     </Alert>
   );
 
   return (
-    console.log(qrResult),
     <div>
       <QrScanner
         onDecode={handleBarcodeScan}
         onError={(error) => console.log(error?.message)}
       />
-       {qrResult && qrResult !== null ? (
-        barcodes.includes(qrResult) ? (
+       {qrResult && qrResult !== null ? ( barcodeExists(qrResult) ? (
           errorAlert
         ) : (
+
           successAlert
+
+  
         )
       ) : null}
 
---
+++ src/BarcodeBlackListComponent.jsx
@@ -0,0 +1,61 @@
+const barcodes = [
+    {
+        "barcode": "649528906526",
+        "barcode_format": "EAN_13",
+        "product_name": "Chipsy Forno",
+        "product_image": "https://www.tesco.com/groceries/en-GB/products/296681279",
+        "product_price": "£1.00",
+        "product_description": "Chipsy Forno 150g",
+    },
+
+    {
+        "barcode": "6221031490569",
+        "barcode_format": "EAN_13",
+        "product_name": "Cimo Cono",
+        "product_image": "https://www.tesco.com/groceries/en-GB/products/296681279",
+        "product_price": "£1.00",
+        "product_description": "Cimo Cono 150g",
+    },
+
+    {
+        "barcode": "6221007083160",
+        "barcode_format": "EAN_13",
+        "product_name": "Nescafe Gold Packet",
+        "product_image": "https://www.tesco.com/groceries/en-GB/products/296681279",
+        "product_price": "£1.00",
+        "product_description": "Nescafe Gold Packet 150g",
+    },
+
+    {
+        "barcode": "6221007022183",
+        "barcode_format": "EAN_13",
+        "product_name": "Nescafe Gold Jar",
+        "product_image": "https://www.tesco.com/groceries/en-GB/products/296681279",
+        "product_price": "£1.00",
+        "product_description": "Nescafe Gold Jar 150g",
+    },
+
+    {
+        "barcode": "8445290212863",
+        "barcode_format": "EAN_13",
+        "product_name": "Heinz Mayonize",
+        "product_image": "https://www.tesco.com/groceries/en-GB/products/296681279",
+        "product_price": "£1.00",
+        "product_description": "Heinz Mayonize 150g",
+    },
+
+    {
+        "barcode": "6221033177239",
+        "barcode_format": "EAN_13",
+        "product_name": "Heinz Ketchup",
+        "product_image": "https://www.tesco.com/groceries/en-GB/products/296681279",
+        "product_price": "£1.00",
+        "product_description": "Heinz Ketchup 150g",
+    },
+];
+
+
+export const BarcodeBlackListComponent = () => barcodes;
+export const barcodeExists = (barcodeToCheck) => barcodes.some(barcode => barcode.barcode === barcodeToCheck);
+
+export const getBarcodeData = (barcodeToCheck) => barcodes.find(barcode => barcode.barcode === barcodeToCheck);
